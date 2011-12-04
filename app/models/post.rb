d8ac8169f253de50045d7353a8583a9dd45ae34c
Justin Herrick <Justin@justinherrick.com>
2011-12-04 12:50:03 -0500

    Hefty initial Commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,19 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :title,    type: String
   field :body,     type: String
   field :category, type: String
   field :pubdate,  type: Date
   field :type,     type: String
   field :notes,    type: String
+  
+  before_save :set_date
+
+
+  private
+
+  def set_date
+  	self.pubdate = self.created_at
+  end
 end
